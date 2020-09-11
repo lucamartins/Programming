917394941ea5af5ed459813d2071f39006894fab
Luca S. Martins <65422994+lucamartinsm@users.noreply.github.com>
2020-09-11 17:13:36 -0300

    up

--- 2_Gilmar/Lista06/03.c
+++ 2_Gilmar/Lista06/03.c
@@ -2,6 +2,15 @@
 
 int main(){
 
+      double numeros[10];
+
+      for(int i=0; i<10; i++){
+            scanf("%lf", &numeros[i]);
+      }
+
+      for(int i=0; i<10; i++){
+            printf("%.2lf / 2 = %.2lf\n", numeros[i], numeros[i]/2);
+      }
 
       return 0;
 }
