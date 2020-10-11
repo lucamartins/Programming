e663ee8856a1a81039ea4572db5e3f61bec9dcc5
Luca S. Martins <65422994+lucamartinsm@users.noreply.github.com>
2020-10-11 10:19:06 -0300

    up

---
+++ 02_Gilmar/Lista10/3_2.c
@@ -0,0 +1,17 @@
+/* Devolve o n-esimo caracter da string s */
+
+#include<stdio.h>
+#include<string.h>
+
+char n_esimo(char *s, int n){
+    return s[n];
+}
+
+int main(){
+    char s[100]; gets(s);
+    int n; scanf("%d", &n);
+
+    printf("%c\n", n_esimo(s, n));
+
+    return 0;
+}
---
+++ 02_Gilmar/Lista10/3_3.c
@@ -0,0 +1,24 @@
+/* Recebe uma string e compacta num unico caractere qualquer conjunto de caracteres repitidos consecutivos */
+
+#include<stdio.h>
+#include<string.h>
+
+char *strpack(char *s){
+    int i,j;
+
+    for(i=j=0; i<strlen(s); i++){
+        if(i==0) s[j++] = s[i];
+        else if(s[i] != s[i-1]) s[j++] = s[i];
+    }
+    s[j] = '\0';
+
+    return s;
+}
+
+int main(){
+    char s[100]; gets(s);
+
+    printf("%s\n", strpack(s));
+
+    return 0;
+}
